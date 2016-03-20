55c3cb3d5a1df563e99ea26712591cd01ce85fa6
Nathan <nathanduprey@hotmail.com>
2016-03-20 00:02:00 -0400

    modularized code a bit

--- code/motor-control/teensy/src/main.cpp
+++ code/motor-control/teensy/src/main.cpp
@@ -3,15 +3,24 @@
 
 // pin defs
 #define PWMA 3
+#define DIRA 5
+#define SLPA 6
 #define PWMB 4
+#define DIRB 7
+#define SLPB 8
 
 // i2c cmd defs
 #define PWMACMD 0x01
-#define PWMBCMD 0x02
-#define 
+#define DIRACMD 0x02
+#define PWMBCMD 0x06
+#define DIRBCMD 0x07
+#define SLPCMD  0x0C
 
 // function prototypes
 void rxEvent(int);
+void setPWM(uint8_t, uint16_t);
+void setDirection(uint8_t, uint16_t);
+void setSleep(uint8_t, uint8_t, uint16_t);
 
 int main(){
 	// set pwm output pins
@@ -39,29 +48,57 @@ int main(){
 }
 
 void rxEvent(int numBytes){
+	// first byte is the command
 	char cmd = Wire.read();
 	uint16_t val = 0;
 	
+	// next two bytes are the value
 	char lowByte = Wire.read();
 	char highByte = Wire.read();
 	val = (highByte << 8) | lowByte;
 	
 	switch(cmd){
 	case PWMACMD:
-		// bound the val
-		if(val > 1024){val = 1024;}
-		else if(val < 0){val = 0;}
-		// set pwm output
-		analogWrite(PWMA, val);
+		setPWM(PWMA, val);
 		break;
 	case PWMBCMD:
-		// bound the value
-		if(val > 1024){val = 1024;}
-		else if(val < 0){val = 0;}
-		// set pwm
-		analogWrite(PWMB, val);
+		setPWM(PWMB, val);
+		break;
+	case DIRACMD:
+		setDirection(PWMA, val);
+		break;
+	case DIRBCMD:
+		setDirection(PWMB, val);
+		break;
+	case SLPCMD:
+		setSleep(SLPA, SLPB, val);
 		break;
 	default:
 		break;
+	}
+}
+
+// function to set the pwm output
+void setPWM(uint8_t channel, uint16_t val){
+	// bound the value
+	if(val > 1023){val = 1023;}
+	else if(val < 0){val = 0;}
+	// set output
+	analogWrite(channel, val);
+}
+
+// function to set the direction of the motor
+void setDirection(uint8_t channel, uint16_t state){
+	if(state == 0 || state == 1){
+		// 0 is forward, 1 is reverse
+		digitalWrite(channel, !state);
+	}
+}
+
+// function to power down or enable the motor drivers
+void setSleep(uint8_t slpA, uint8_t slpB, uint16_t state){
+	if(state == 0 || state == 1){
+		digitalWrite(slpA, state);
+		digitalWrite(slpB, state);
 	}
 }
