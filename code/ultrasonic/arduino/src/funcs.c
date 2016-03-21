cfe50c52af279bca3ddd2b0d7b201a14eea1d614
Nathan <nathanduprey@hotmail.com>
2016-03-21 17:40:41 -0400

    interrupts configured

--- code/ultrasonic/arduino/src/funcs.c
+++ code/ultrasonic/arduino/src/funcs.c
@@ -7,12 +7,22 @@ void pinInit(void){
   DDRB &= ~(SENS1);
   DDRC &= ~(SENS2);
   DDRD &= ~(SENS3 | SENS4);
-  
+
   PORTB |= (SENS1);
   PORTC |= (SENS2);
   PORTD |= (SENS3 | SENS4);
 }
 
 void interruptInit(void){
+  // pin change mask register, enable interrupts on corresponding pins
+  PCMSK0 |= (1<<PCINT0);
+  PCMSK1 |= (1<<PCINT8);
+  PCMSK2 |= (1<<PCINT16);
+  // pin change interrupt control register, enable PCIE0-2
+  PCICR |= (1<<PCIE0 | 1<<PCIE1 | 1<<PCIE2);
 
+  // external inperrupt mask register, enable interrupt on corresponding pins
+  EIMSK |= (1<<INT0);
+  // external interrupt control register, generate interrupt on change
+  EICRA |= (1<<ISC00);
 }
--- code/ultrasonic/arduino/src/main.c
+++ code/ultrasonic/arduino/src/main.c
@@ -5,3 +5,23 @@
 int main(void){
 
 }
+
+// isr for sens1
+ISR(PCINT0_vect){
+
+}
+
+// isr for sens2
+ISR(PCINT1_vect){
+
+}
+
+// isr for sens3
+ISR(PCINT2_vect){
+
+}
+
+// isr for sens4
+ISR(INT0_vect){
+
+}
